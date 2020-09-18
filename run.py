99334bd8607e60c91c6a1bd21c679733876e8ec0
David Ficociello <davidfic@pop-os.localdomain>
2020-09-18 13:31:45 -0400

    fixing typo

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from save_spend_share import app
 # testing out some drone fun.....more
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
